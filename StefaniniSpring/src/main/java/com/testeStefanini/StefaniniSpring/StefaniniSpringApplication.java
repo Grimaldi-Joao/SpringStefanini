ce037da13e3583db49319832775f177f1e5a8acf
Grimaldi-Joao <jaoagrimaldi6@gmail.com>
2024-05-23 16:41:42 -0300

    feat: iniciar projeto com configuração básica

---
+++ StefaniniSpring/src/main/java/com/testeStefanini/StefaniniSpring/StefaniniSpringApplication.java
@@ -0,0 +1,13 @@
+package com.testeStefanini.StefaniniSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StefaniniSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StefaniniSpringApplication.class, args);
+	}
+
+}
---
+++ StefaniniSpring/src/test/java/com/testeStefanini/StefaniniSpring/StefaniniSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeStefanini.StefaniniSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StefaniniSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

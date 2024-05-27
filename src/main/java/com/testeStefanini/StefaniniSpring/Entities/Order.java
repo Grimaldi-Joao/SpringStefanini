d588c2b8ae02814e454b1973163b5ab6ff60e737
Grimaldi-Joao <joaogrimaldi6@gmail.com>
2024-05-27 15:01:10 -0300

    Feat: Implementação das anotations em Order.java

--- src/main/java/com/testeStefanini/StefaniniSpring/Entities/Order.java
+++ src/main/java/com/testeStefanini/StefaniniSpring/Entities/Order.java
@@ -1,15 +1,26 @@
 package com.testeStefanini.StefaniniSpring.Entities;
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 
+@Entity
 public class Order implements Serializable{
-    
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
 
+    @ManyToOne//Anotation que serve para mostrar que a variavel cliente é uma chave de fora e alem disso mostrar que a relação é de varios pedidos para 1
+    @JoinColumn(name = "client_id")//criar uma coluna com o nome desiguinado
     private User client;
 
     public Order(){
@@ -40,20 +51,28 @@ public class Order implements Serializable{
         this.moment = moment;
     }
 
-    @Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime*result+((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		return Objects.equals(id, other.id);
-	}
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if(other.getId() != null)
+                return false;
+        }else if(!id.equals(other.getId())){
+            return false;
+        }
+        return true;
+    }
 }

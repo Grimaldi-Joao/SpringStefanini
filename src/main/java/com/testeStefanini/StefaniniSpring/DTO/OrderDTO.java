f3af26310c45866c64a66b8400c60720bfe3ce31
Grimaldi-Joao <joaogrimaldi6@gmail.com>
2024-06-07 18:48:42 -0300

    WIP: DTO order implementado, um pouco de dificuldade para entender o conseito de DTO

--- src/main/java/com/testeStefanini/StefaniniSpring/DTO/OrderDTO.java
+++ src/main/java/com/testeStefanini/StefaniniSpring/DTO/OrderDTO.java
@@ -1,5 +1,61 @@
 package com.testeStefanini.StefaniniSpring.DTO;
 
+import java.time.Instant;
+
 public class OrderDTO {
-    
+    private Long id;
+    private Instant moment;
+    private String orderStatus;
+    private String clientName;
+    private Double total;
+
+    public OrderDTO() {}
+
+    public OrderDTO(Long id, Instant moment, String orderStatus, String clientName, Double total) {
+        this.id = id;
+        this.moment = moment;
+        this.orderStatus = orderStatus;
+        this.clientName = clientName;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
 }
